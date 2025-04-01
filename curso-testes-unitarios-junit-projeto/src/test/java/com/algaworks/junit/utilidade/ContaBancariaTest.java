8786a3b2de96dd08de44ec20f2f532673aaf4145
sampa <sampaio_pedro@outlook.com>
2025-04-01 17:57:24 -0300

    Subindo

--- curso-testes-unitarios-junit-projeto/src/test/java/com/algaworks/junit/utilidade/ContaBancariaTest.java
+++ curso-testes-unitarios-junit-projeto/src/test/java/com/algaworks/junit/utilidade/ContaBancariaTest.java
@@ -1,5 +1,7 @@
 package com.algaworks.junit.utilidade;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -7,86 +9,98 @@ import java.math.BigDecimal;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ContaBancariaTest {
-    @Test
-    void deveInformarValorValidoSaldo() {
-        BigDecimal valorSaldo = new BigDecimal(300);
-        ContaBancaria contaBancaria = new ContaBancaria(valorSaldo);
-        assertEquals(valorSaldo, contaBancaria.saldo());
+    @Nested
+    @DisplayName("Testes de saldo")
+    class saldo {
+        @Test
+        void deveInformarValorValidoSaldo() {
+            BigDecimal valorSaldo = new BigDecimal(300);
+            ContaBancaria contaBancaria = new ContaBancaria(valorSaldo);
+            assertEquals(valorSaldo, contaBancaria.saldo());
+        }
+
+        @Test
+        void deveLancarExeptionValorInvalidoSaldo() {
+            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                    () -> new ContaBancaria(null));
+
+            assertEquals("Valor de saldo não pode ser nulo", illegalArgumentException.getMessage());
+        }
     }
 
-    @Test
-    void deveLancarExeptionValorInvalidoSaldo() {
-        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
-                () -> new ContaBancaria(null));
+    @Nested
+    @DisplayName("Testes de Saque")
+    class saque {
+        @Test
+        void deveRealizarSaque() {
+            BigDecimal valorSaldo = new BigDecimal(300);
+            ContaBancaria contaBancaria = new ContaBancaria(valorSaldo);
+            BigDecimal valorSaque = new BigDecimal(200);
+            contaBancaria.saque(valorSaque);
+            assertEquals(valorSaldo.subtract(valorSaque), contaBancaria.saldo());
+        }
 
-        assertEquals("Valor de saldo não pode ser nulo", illegalArgumentException.getMessage());
+        @Test
+        void naoDeveRealizarSaqueComValorNull() {
+            BigDecimal valorSaldo = new BigDecimal(300);
+            ContaBancaria contaBancaria = new ContaBancaria(valorSaldo);
+            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                    () -> contaBancaria.saque(null));
+
+            assertEquals("Valor não pode ser nulo nem menor igual a zero", illegalArgumentException.getMessage());
+        }
+
+        @Test
+        void naoDeveRealizarSaqueComValorMenorIgualZero() {
+            BigDecimal valorSaldo = new BigDecimal(300);
+            ContaBancaria contaBancaria = new ContaBancaria(valorSaldo);
+            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                    () -> contaBancaria.saque(new BigDecimal(0)));
+
+            assertEquals("Valor não pode ser nulo nem menor igual a zero", illegalArgumentException.getMessage());
+        }
+        @Test
+        void naoDeveRealizarSaqueComValorMaiorSaldo() {
+            BigDecimal valorSaldo = new BigDecimal(300);
+            ContaBancaria contaBancaria = new ContaBancaria(valorSaldo);
+            RuntimeException runtimeException = assertThrows(RuntimeException.class,
+                    () -> contaBancaria.saque(new BigDecimal(301)));
+
+            assertEquals("Valor de saque/deposito maior que valor de saldo", runtimeException.getMessage());
+        }
+
     }
 
-    @Test
-    void deveRealizarSaque() {
-        BigDecimal valorSaldo = new BigDecimal(300);
-        ContaBancaria contaBancaria = new ContaBancaria(valorSaldo);
-        BigDecimal valorSaque = new BigDecimal(200);
-        contaBancaria.saque(valorSaque);
-        assertEquals(valorSaldo.subtract(valorSaque), contaBancaria.saldo());
-    }
+    @Nested
+    @DisplayName("Testes de Saque")
+    class deposito {
 
-    @Test
-    void naoDeveRealizarSaqueComValorNull() {
-        BigDecimal valorSaldo = new BigDecimal(300);
-        ContaBancaria contaBancaria = new ContaBancaria(valorSaldo);
-        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
-                () -> contaBancaria.saque(null));
+        @Test
+        void deveRealizarDeposito() {
+            BigDecimal valorSaldo = new BigDecimal(300);
+            ContaBancaria contaBancaria = new ContaBancaria(valorSaldo);
+            BigDecimal valorDeposito = new BigDecimal(200);
+            contaBancaria.deposito(valorDeposito);
+            assertEquals(valorSaldo.add(valorDeposito), contaBancaria.saldo());
+        }
 
-        assertEquals("Valor não pode ser nulo nem menor igual a zero", illegalArgumentException.getMessage());
-    }
+        @Test
+        void naoDeveRealizarDepositoComValorNull() {
+            BigDecimal valorSaldo = new BigDecimal(300);
+            ContaBancaria contaBancaria = new ContaBancaria(valorSaldo);
+            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                    () -> contaBancaria.deposito(null));
 
-    @Test
-    void naoDeveRealizarSaqueComValorMenorIgualZero() {
-        BigDecimal valorSaldo = new BigDecimal(300);
-        ContaBancaria contaBancaria = new ContaBancaria(valorSaldo);
-        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
-                () -> contaBancaria.saque(new BigDecimal(0)));
+            assertEquals("Valor não pode ser nulo nem menor igual a zero", illegalArgumentException.getMessage());
+        }
+        @Test
+        void naoDeveRealizarDepositoComValorMenorIgualZero() {
+            BigDecimal valorSaldo = new BigDecimal(300);
+            ContaBancaria contaBancaria = new ContaBancaria(valorSaldo);
+            IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                    () -> contaBancaria.saque(new BigDecimal(0)));
 
-        assertEquals("Valor não pode ser nulo nem menor igual a zero", illegalArgumentException.getMessage());
-    }
-
-    @Test
-    void naoDeveRealizarSaqueComValorMaiorSaldo() {
-        BigDecimal valorSaldo = new BigDecimal(300);
-        ContaBancaria contaBancaria = new ContaBancaria(valorSaldo);
-        RuntimeException runtimeException = assertThrows(RuntimeException.class,
-                () -> contaBancaria.saque(new BigDecimal(301)));
-
-        assertEquals("Valor de saque/deposito maior que valor de saldo", runtimeException.getMessage());
-    }
-
-    @Test
-    void deveRealizarDeposito() {
-        BigDecimal valorSaldo = new BigDecimal(300);
-        ContaBancaria contaBancaria = new ContaBancaria(valorSaldo);
-        BigDecimal valorDeposito = new BigDecimal(200);
-        contaBancaria.deposito(valorDeposito);
-        assertEquals(valorSaldo.add(valorDeposito), contaBancaria.saldo());
-    }
-
-    @Test
-    void naoDeveRealizarDepositoComValorNull() {
-        BigDecimal valorSaldo = new BigDecimal(300);
-        ContaBancaria contaBancaria = new ContaBancaria(valorSaldo);
-        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
-                () -> contaBancaria.deposito(null));
-
-        assertEquals("Valor não pode ser nulo nem menor igual a zero", illegalArgumentException.getMessage());
-    }
-
-    @Test
-    void naoDeveRealizarDepositoComValorMenorIgualZero() {
-        BigDecimal valorSaldo = new BigDecimal(300);
-        ContaBancaria contaBancaria = new ContaBancaria(valorSaldo);
-        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
-                () -> contaBancaria.saque(new BigDecimal(0)));
-
-        assertEquals("Valor não pode ser nulo nem menor igual a zero", illegalArgumentException.getMessage());
+            assertEquals("Valor não pode ser nulo nem menor igual a zero", illegalArgumentException.getMessage());
+        }
     }
 }
--- curso-testes-unitarios-junit-projeto/src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ curso-testes-unitarios-junit-projeto/src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -2,55 +2,81 @@ package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.function.Executable;
+
+import static com.algaworks.junit.utilidade.SaudacaoUtil.saudar;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Testes de saudações")
 class SaudacaoUtilTest {
     @Test
     @DisplayName("Testando sucesso da mensagem de bom dia, a partir das 5h")
     public void saudarBomDiaAApartir5h() {
-        String saudacao = SaudacaoUtil.saudar(5);
+        // Padrão Tiple A
+
+        // Arrange
+        int horaValida = 5;
+
+        // Act
+        String saudacao = saudar(horaValida);
+
+        // Assert
         assertEquals("Bom dia", saudacao, "Saudação de bom dia incorreta");
     }
 
     @Test
     @DisplayName("Testando sucesso da mensagem de bom dia")
     public void saudarBomDia() {
-        String saudacao = SaudacaoUtil.saudar(9);
+        String saudacao = saudar(9);
         assertEquals("Bom dia", saudacao, "Saudação de bom dia incorreta");
     }
 
     @Test
     @DisplayName("Testando sucesso da mensagem de boa tarde")
     public void saudarBoaTarde() {
-        String saudacao = SaudacaoUtil.saudar(15);
+        String saudacao = saudar(15);
         assertEquals("Boa tarde", saudacao, "Saudação de boa tarde incorreta");
     }
 
     @Test
     @DisplayName("Testando sucesso da mensagem de boa noite")
     public void saudarBoaNoite() {
-        String saudacao = SaudacaoUtil.saudar(21);
+        String saudacao = saudar(21);
         assertEquals("Boa noite", saudacao, "Saudação de boa noite incorreta");
     }
 
     @Test
     @DisplayName("Testando sucesso da mensagem de boa noite, até as 4h")
     public void saudarBoaNoiteAte4h() {
-        String saudacao = SaudacaoUtil.saudar(4);
+        String saudacao = saudar(4);
         assertEquals("Boa noite", saudacao, "Saudação de boa noite incorreta");
     }
 
     @Test
     @DisplayName("Testando sucesso da exception")
     public void deveLancarException() {
-        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
-                () -> SaudacaoUtil.saudar(-10));
+        int horaInvalida = -10;
+
+        // Jeito I
+        // IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+        //         () -> saudar(horaInvalida));
+
+        // Jeito II
+        Executable executable = () -> saudar(horaInvalida);
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, executable);
+
+
         assertEquals("Hora inválida", illegalArgumentException.getMessage());
     }
 
     @Test
     @DisplayName("Testando o que não deve lançar uma exception")
     public void naoDeveLancarException(){
-        assertDoesNotThrow(() -> SaudacaoUtil.saudar(0));
+        int horaValida = 0;
+
+        Executable executable = () -> saudar(horaValida);
+
+        assertDoesNotThrow(executable);
     }
 }
